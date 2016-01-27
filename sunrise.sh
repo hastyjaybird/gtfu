fb93fea7eff8a350722e73d0c35ce19993032342
JayBird <jaybird@debian>
2016-01-26 17:06:59 -0800

    this waits until sunrise, should work!

--- sunrise.sh
+++ sunrise.sh
@@ -1,3 +1,9 @@
 curl www.sunrisesunset.com >/tmp/whatever
-cat /tmp/whatever | grep "San Francisco" | cut -d: -f 3
-cat /tmp/whatever | grep "San Francisco" | cut -d: -f 4 | cut -da -f 1
+HOUR=`cat /tmp/whatever | grep "San Francisco" | cut -d: -f 3`
+MIN=`cat /tmp/whatever | grep "San Francisco" | cut -d: -f 4 | cut -da -f 1`
+echo Sunrise will be at: $HOUR:$MIN
+WAITH=$(($HOUR-1))
+echo Sleeping for "$WAITH"hours and "$MIN". ZZZ
+sleep "$WAITH"h "$MIN"m
+stty /dev/ttyATH0 300
+echo r > /dev/ttyATH0
