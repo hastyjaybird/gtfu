0cde2f7ac954ced83776a6161dc39c6da7c5918c
Jay H <Jay H>
2016-01-26 23:57:27 -0800

    switch serial ports

--- gtfu.ino
+++ gtfu.ino
@@ -3,19 +3,23 @@
 #define LEDPIN 11    // LED connected to digital pin 11
 #define MAXPWM 169   // this PWM corresponds to 3.3v with a 5v power supply
 #define MINPWM 3   // this PWM is when the lights first start to light up
-#define FADETIME 60 * 60 * 1000
+#define FADETIME 6000 // * 60 * 1000
 #define DELAYVALUE FADETIME / ( MAXPWM - MINPWM )
+#define SERIAL Serial1 //this is for OpenWRT
+//#define SERIAL Serial //this is for usb connection
+
+
 
 void setup() {
-  Serial.begin(300);
+  SERIAL.begin(300);
   pinMode(LEDPIN,OUTPUT);
   setPwmFrequency(LEDPIN,1); // divisor of 1 is probably 31250 Hz
 }
 
 void loop() {
-  if (Serial.available()) {
-    char readChar = Serial.read();
-    Serial.println(readChar);
+  if (SERIAL.available()) {
+    char readChar = SERIAL.read();
+    SERIAL.println(readChar);
     if (readChar == 'r'){ // sunrise
       sunrise();
     }
@@ -94,6 +98,6 @@ void setPwmFrequency(int pin, int divisor) {
     default:
       return;
     }
-    TCCR2B = TCCR2B & 0b11111000 | mode;
+//    TCCR2B = TCCR2B & 0b11111000 | mode;
   }
 }
